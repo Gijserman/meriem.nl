0c248ecb33240d9d4ead251511f30f6e0125ae90
Gijs Kattenberg <g.kattenberg@atabix.nl>
2025-01-16 22:11:39 +0100

    New contact text

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,12 +2,10 @@ export default function Contact() {
     return (
         <>
             <p>
-                Bel of mail me vrijblijvend, zodat we kunnen bekijken wat ik voor jullie kan betekenen. Zorg zowel op
+                Bel me op 06 145 29 630 of stuur een bericht naar mail@meriem.nl, zodat we kunnen bekijken wat ik voor
+                jullie kan betekenen. Zorg zowel op
                 school, thuis, als op de peuterspeelzaal. Ik ben inzetbaar in de regio Amsterdam, Weesp en Muiden.
             </p>
-            <address>
-                Heb je vragen? Bel me op <strong>06 145 29 630</strong>
-            </address>
         </>
     )
         ;
