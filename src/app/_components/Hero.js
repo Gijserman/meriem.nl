8d7bedf0de574a0b8e27de3eeed199f87109a95e
Gijs Kattenberg <g.kattenberg@atabix.nl>
2025-01-16 09:25:12 +0100

    logging

--- src/app/_components/Hero.js
+++ src/app/_components/Hero.js
@@ -74,6 +74,7 @@ export default function Hero({ isVisible = true }) {
     const [rightImage, setRightImage] = useState("/video/wie-ben-ik@2x.mp4.jpg");
 
     useEffect(() => {
+        console.log(currentIndex);
         transitions.slideIntoViewport();
         setVideo(`/video${pageKeys[currentIndex]}@2x.mp4`);
         setLeftImage(`/video${pageKeys[previousIndex]}@2x.mp4.jpg`);
