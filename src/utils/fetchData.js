954ee7b299cafc092b29dcd34a92a485c7ade007
Gijs Kattenberg <g.kattenberg@atabix.nl>
2025-02-02 22:47:19 +0100

    Rename env

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,6 @@
 import { buildClient } from '@datocms/cma-client-node';
 
-const client = buildClient({ apiToken: process.env.DATOCMS_API_TOKEN });
+const client = buildClient({ apiToken: process.env.DATOCMS_READONLY_TOKEN });
 
 export async function fetchData(type) {
     return await client.items.list({
