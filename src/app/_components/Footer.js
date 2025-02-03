dd28f2d3e0f57808b5b227b164eb95264e1aeb1e
Gijs Kattenberg <g.kattenberg@atabix.nl>
2025-02-03 16:14:35 +0100

    h2

--- src/app/_components/Footer.js
+++ src/app/_components/Footer.js
@@ -10,9 +10,9 @@ export default async function Footer() {
             <section className="w-full max-w-[1024px] grid grid-cols-1 md:grid-cols-3 gap-8 py-8 border-b-2">
                 {footerColumns.map((item) => (
                     <div key={item.id} className="text-center md:text-left">
-                        <h3 className="text-red-500 font-semibold text-lg mb-2 max-w-[300px] mx-auto">
+                        <h2 className="text-red-500 font-semibold text-lg mb-2 max-w-[300px] mx-auto">
                             {item.title}
-                        </h3>
+                        </h2>
                         <Markdown
                             className="text-gray-700 leading-loose max-w-[300px] mx-auto"
                             remarkPlugins={[remarkBreaks]}
