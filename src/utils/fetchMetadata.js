46c7bbb6cf427f553861d6907d508b577aa70c30
Gijs Kattenberg <g.kattenberg@atabix.nl>
2025-02-03 16:43:40 +0100

    Fix meta

--- src/utils/fetchMetadata.js
+++ src/utils/fetchMetadata.js
@@ -1,17 +1,18 @@
 import {fetchData} from "./fetchData";
 
 export async function fetchMetadata(type) {
-    const page = await fetchData(type);
+    const pageData = await fetchData(type);
+    const page = pageData?.[0];
     const canonicalUrl = `${process.env.NEXT_BASE_URL}/${type}`;
 
     return {
-        title: page.title,
-        description: page.content,
+        title: page?.title || 'Default Title',
+        description: page?.content || 'Default description', // Fallback if content is missing
         alternates: {
             canonical: canonicalUrl,
         },
         openGraph: {
-            title: page.title,
+            title: page?.title || 'Default Title',
             type: 'website',
             url: canonicalUrl,
             images: [
@@ -19,7 +20,7 @@ export async function fetchMetadata(type) {
                     url: `/video/${type}@2x.mp4.jpg`,
                     width: 630,
                     height: 630,
-                    alt: page.title,
+                    alt: page?.title || 'Default Alt Text',
                 },
             ],
         },
